99b2ce859523d485a4bf1e837c44aede2be7f938
yunusemre12500 <131378214+yunusemre12500@users.noreply.github.com>
2025-03-19 21:45:44 +0300

    release(userrepository): release v1

---
+++ repository/userrepository/user_repository.go
@@ -0,0 +1,16 @@
+package userrepository
+
+import (
+	"context"
+
+	"github.com/yunusemre12500/todo-app/model"
+	"github.com/yunusemre12500/todo-app/repository"
+)
+
+type UserRepository interface {
+	repository.Repository
+	DeleteUserByID(context.Context, model.ID) error
+	GetUserByID(context.Context, model.ID) (*model.User, error)
+	GetUserByName(context.Context, string) (*model.User, error)
+	SaveUser(context.Context, *model.User) error
+}
